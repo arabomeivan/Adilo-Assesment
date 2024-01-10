66f670ccbd26491474d54af4124ec17cb995ca64
e4ewebdev <arabomeivan@protonmail.com>
2024-01-10 08:03:26 +0100

    added assets

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,10 @@
 </div>
 <!-- RECORD SECTION -->
 <div class="flex-grow-1 border record-section mt-5 p-5 text-center">
+
+  <div class="text-center mb-2">
+ <img src="../assets/images/folder.png" class="img-fluid" alt="">
+  </div>
 <h5 class="text-black">Say hello to the world!</h5>
 
 <p class="text-grey text-small mt-1">Record your first video/audio and share it what your team, friends, followers and customers.</p>
@@ -27,7 +31,7 @@
 <!-- INFO SECTION -->
 <div class="row mt-2">
 <div class="col-lg-6">
-
+<img src="../assets/images/window-preview.png" class="img-fluid mt-3" alt="">
 </div>
 
 <div class="col-lg-6 py-5">
